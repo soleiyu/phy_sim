80f25a91d5427596a4a1f1913131927da839ea14
soleiyu <ttahr1404@gmail.com>
2020-12-22 14:40:55 +0900

    backup1223

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import "./lib"
 
 func main() {
 
-	vx := 2.0
+	vx := 3.0
 
 	m1 := lib.NewMaterial(0, 0)
-	m2 := lib.NewMaterial(4, -4)
+	m2 := lib.NewMaterial(0.2, -0.2)
 
 //	ps := lib.NewParticleArray5x9(vx)
-	ps := lib.NewParticleArray(11, 9, 8, 4, -9, vx)
+	ps := lib.NewParticleArray(11, 9, 8, 4, -10, vx)
 	ms := []lib.Material{m1, m2}
 
 	lib.PrintCoodPsMs(ps, ms)
